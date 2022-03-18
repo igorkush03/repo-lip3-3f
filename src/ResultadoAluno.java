502e1765651c54216bbf447c18cd0e95209d6ef7
Igor Gabriel Nascimento Domingues <rm74618@estudante.fieb.edu.br>
2022-03-18 12:16:52 -0300

    Primeiro Commit

---
+++ src/ResultadoAluno.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+		
+		double media = ?;
+		
+	
+		
+		if( 6 > y ) {
+			JOptionPane.showMessageDialog(null,
+					x + " é a maior que " + y);
+		} else if ( 6 < y ) {
+			JOptionPane.showMessageDialog(null,
+					6 + "é menor que " + y);
+
+	}
+
+}
